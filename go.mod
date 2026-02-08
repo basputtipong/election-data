ffbf73ad886877537af86b119204e07735a57712
bas puttipong <basputtipongz@gmail.com>
2026-02-09 02:59:05 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module election-data
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func download(url, file string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func main() {
+	download(
+		"https://stats-ectreport69.ect.go.th/data/records/stats_cons.json",
+		"data/stats_cons.json",
+	)
+	download(
+		"https://static-ectreport69.ect.go.th/data/data/refs/info_party_overview.json",
+		"data/info_party_overview.json",
+	)
+	download(
+		"https://stats-ectreport69.ect.go.th/data/records/stats_party.json",
+		"data/stats_party.json",
+	)
+	download(
+		"https://static-ectreport69.ect.go.th/data/data/refs/info_province.json",
+		"data/info_province.json",
+	)
+}
